18a70ff5094a4dec901510e19111a85da05a765b
AdamMeyers42 <adammeyers3551@gmail.com>
2021-10-11 08:27:08 -0700

    Put choose_gesture in parent Player class, tested that method on both human and ai child classes.

--- ai.py
+++ ai.py
@@ -1,7 +1,12 @@
 from player import Player
+import random
 
 class AI(Player):
     def __init__(self):
         super().__init__()
+
+    def choose_gesture(self):
+        self.gestures = print(random.choices(self.gestures_list))
+
 
 
--- gameboard.py
+++ gameboard.py
@@ -1,9 +1,14 @@
 from human import Human
+from ai import AI
 
-human_1 = Human()
-human_1.gestures_list
-for gesture in human_1.gestures_list:
-    print(gesture)
+# human_1 = Human()
+# human_1.gestures_list
+# for gesture in human_1.gestures_list:
+#     print(gesture)
+
+# human_1.choose_gesture()
 
 
-human_1.choose_gesture()
+
+computer_1 = AI()
+computer_1.choose_gesture()
